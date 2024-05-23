b8726a777321375a8792714015a71d91a882381b
PeterXRiber <90039743+PeterXRiber@users.noreply.github.com>
2024-05-23 22:07:33 +0200

    Passed the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array=[], removeValue) {
 
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] === removeValue) {
+
+            array.splice(i,1);
+        }
+        
+    }
+    
+    return array;
 };
+
+removeFromArray();
 
 // Do not edit below this line
 module.exports = removeFromArray;
