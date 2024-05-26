f363faa5e4ea0654a63bb248ec7b023541a1270f
PeterXRiber <90039743+PeterXRiber@users.noreply.github.com>
2024-05-26 11:39:37 +0200

    code works with any positive number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let sum = 0;
+
+    let biggestInt;
+    let smallestInt;
+
+    if (int1 > int2) {
+        biggestInt = int1;
+        smallestInt = int2;
+    } else {
+        biggestInt = int2;
+        smallestInt = int1;
+    }
+
+    for (smallestInt; smallestInt <= biggestInt; smallestInt++) {
+        
+        
+
+        sum += smallestInt;
+
+      
+    }
+    
+    return sum;
 };
+
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
