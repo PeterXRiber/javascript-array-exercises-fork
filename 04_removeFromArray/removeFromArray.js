439c7f21ce3fa94d9be548c358942e0c336d4836
PeterXRiber <90039743+PeterXRiber@users.noreply.github.com>
2024-05-26 10:38:14 +0200

    Added comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,14 @@
 const removeFromArray = function(array=[], ...removeValues) {
 
-    
-
     return array.filter(x => !removeValues.includes(x));
 };
-
 removeFromArray();
 
+
+  // x => is the same as writing "function (x) {}" but simply more concise
+  // the return statement states in words 
+  // "Filter the array where x represents values to keep.
+  //  Do not remove values that is included in x. Aka. do not remove values from the first parameter".
+  
 // Do not edit below this line
 module.exports = removeFromArray;
