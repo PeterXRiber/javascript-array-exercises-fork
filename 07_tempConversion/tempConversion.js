18dacf2b19d4142805fa63491280d9d6733019ec
PeterXRiber <90039743+PeterXRiber@users.noreply.github.com>
2024-05-28 09:06:35 +0200

    task complete!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+
+
+
+const convertToCelsius = function(tempFahrenheit) {
+  let fahrenheitToCelcius = (tempFahrenheit - 32) * 5/9;
+  tempFahrenheit = fahrenheitToCelcius;
+  tempFahrenheit = Number(tempFahrenheit.toFixed(1));
+
+  return tempFahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+  let celciusToFahrenheit = tempCelcius * 9/5 + 32;
+  tempCelcius = celciusToFahrenheit;
+  tempCelcius = Number(tempCelcius.toFixed(1));
+
+  return tempCelcius;
 };
+
+convertToCelsius();
+convertToFahrenheit();
 
 // Do not edit below this line
 module.exports = {
